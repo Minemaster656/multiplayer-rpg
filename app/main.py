a87a588ee55ed3f2d667e0ebe37fb8609400bd20
Minemaster656 <glebbasmanov123@gmail.com>
2025-12-23 22:32:20 +0400

    Статик папка

--- app/main.py
+++ app/main.py
@@ -1,12 +1,21 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 from app.routers import users, items  # Импорты модулей
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
 
 app = FastAPI(title="My Modular API")
+app.mount("/static", StaticFiles(directory="app/static"))
+templates = Jinja2Templates(directory="app/templates")
 
 app.include_router(users.router, prefix="/users", tags=["users"])
 app.include_router(items.router, prefix="/items", tags=["items"])
 
 
 @app.get("/")
-def read_root():
-    return {"message": "OK"}
+def read_root(request: Request):
+    return templates.TemplateResponse(
+        "index.html",
+        {
+            "request": request,
+        },
+    )
